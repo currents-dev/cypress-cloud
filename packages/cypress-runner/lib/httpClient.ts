a9711bde1fbb2cd37dbc8979593159d183bfa866
vCaisim <slavic.caisim@opentech.dev>
2022-12-12 18:37:25 +0200

    feat: implement generic http client with retries

---
+++ packages/cypress-runner/lib/httpClient.ts
@@ -0,0 +1,77 @@
+import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
+
+type RetryOptions = {
+  delays?: number[];
+  isRetriableError?: (...args: any[]) => boolean;
+};
+
+export const makeRequest = <T = any, D = any>(
+  config: AxiosRequestConfig<D>,
+  retryOptions?: RetryOptions
+) => {
+  return retryWithBackoff(
+    (retryIndex: number) =>
+      axios({
+        baseURL: "http://localhost:1234/",
+        headers: {
+          "x-cypress-request-attempt": retryIndex,
+          ...config.headers,
+        },
+        ...config,
+      }),
+    retryOptions
+  ) as Promise<AxiosResponse<T, D>>;
+};
+
+const DELAYS = [30 * 1000, 60 * 1000, 2 * 60 * 1000]; // 30s, 1min, 2min
+
+const isRetriableError = (err: { response?: { status?: number } }) => {
+  return (
+    err?.response?.status &&
+    500 <= err.response.status &&
+    err.response.status < 600
+  );
+};
+
+const retryWithBackoff = (fn: Function, retryOptions?: RetryOptions) => {
+  let attempt: any;
+
+  const options = {
+    delays: DELAYS,
+    isRetriableError: isRetriableError,
+    ...retryOptions,
+  };
+
+  return (attempt = (retryIndex: number) => {
+    return promiseTry(() => fn(retryIndex)).catch((err) => {
+      if (!options.isRetriableError(err)) throw err;
+
+      if (retryIndex > options.delays.length) {
+        throw err;
+      }
+
+      const delay = options.delays[retryIndex];
+
+      console.warn("API failed retrying", {
+        delay,
+        tries: options.delays.length - retryIndex,
+        response: err,
+      });
+
+      retryIndex++;
+
+      return promiseDelay(delay).then(() => {
+        console.debug(`Retry #${retryIndex} after ${delay}ms`);
+
+        return attempt(retryIndex);
+      });
+    });
+  })(0);
+};
+
+const promiseTry = (fn: Function) => {
+  return new Promise((resolve) => resolve(fn()));
+};
+
+const promiseDelay = (duration: number) =>
+  new Promise((resolve) => setTimeout(resolve, duration));
