88d5ed29cef9b06b2e344d171b182201e7733cf4
Richard Thornton <7372040+richthornton@users.noreply.github.com>
2022-12-12 21:13:36 -0800

    Get platform os information

--- packages/cypress-runner/index.ts
+++ packages/cypress-runner/index.ts
@@ -19,6 +19,7 @@ import {
   getCiParams,
   getCiProvider,
 } from "./lib/ciProvider";
+import { getPlatformInfo } from "./lib/platform";
 
 const stdout = capture.stdout();
 
@@ -78,9 +79,10 @@ export async function run() {
     process.exit(0);
   }
 
+  const osPlatformInfo = await getPlatformInfo();
+
   const platform = {
-    osName: "darwin",
-    osVersion: "22.1.0",
+    ...osPlatformInfo,
     browserName: "Electron",
     browserVersion: "106.0.5249.51",
   };
