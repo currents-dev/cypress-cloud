67362356ea6ff6e86a201c96176ee8c0f376032f
Andrew Goldis <agoldis@gmail.com>
2022-12-13 23:02:07 -0800

    chore: add testing

--- packages/cypress-runner/jest.config.js
+++ packages/cypress-runner/jest.config.js
@@ -2,6 +2,9 @@ module.exports = {
   verbose: true,
   testEnvironment: "node",
   testMatch: ["<rootDir>/**/__tests__/**/?(*.)(spec|test).ts"],
+  transform: {
+    "^.+\\.tsx?$": "jest-esbuild",
+  },
   transformIgnorePatterns: ["node_modules"],
   moduleFileExtensions: ["ts", "js", "d.ts"],
 };
