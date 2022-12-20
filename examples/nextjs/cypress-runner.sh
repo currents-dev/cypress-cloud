ae7e6bc72f3f446b7e4ad055ddab30dcc6ae07ec
Richard Thornton <7372040+richthornton@users.noreply.github.com>
2022-12-19 16:40:42 -0800

    Remove inlined key

--- examples/nextjs/cypress-runner.sh
+++ examples/nextjs/cypress-runner.sh
@@ -2,4 +2,4 @@
 
 export CURRENTS_API_BASE_URL=http://localhost:1234
 
-yarn cypress-cloud --parallel --record --key 3RvHxaHTXzeOWZiT --ci-build-id $(date +%s)
+yarn cypress-cloud --parallel --record --key ${CURRENTS_RECORD_KEY} --ci-build-id $(date +%s)
