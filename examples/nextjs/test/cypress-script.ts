929f3c90f3cb14807f0eb584c3dec0198bfb8381
Vitalii Melnychuk <vvv.melnychuk@gmail.com>
2022-12-29 07:29:50 +0000

    feat: add more info about script params (#57)
    * feat: add more info about script params
    
    * chore: copy
    
    Co-authored-by: Andrew Goldis <agoldis@gmail.com>

--- examples/nextjs/test/cypress-script.ts
+++ examples/nextjs/test/cypress-script.ts
@@ -1,10 +1,29 @@
-import { run } from "cypress-runner";
+const { loadEnvConfig } = require("@next/env");
+import { run } from "cypress-cloud";
+
+function loadEnvVariables() {
+  const projectDir = process.cwd();
+  loadEnvConfig(projectDir);
+}
+
+loadEnvVariables();
 
 (async function runTests() {
-  const projectId = "your-project-id";
-  const key = "your-project-id";
-  const specPattern = "cypress/e2e/1000.spec.js";
-  const summarizedResults = await run({ projectId, key, specPattern });
+  const projectId = process.env.CURRENTS_PROJECT_ID || "";
+  const key = process.env.CURRENTS_RECORD_KEY || "";
+
+  const spec = ["cypress/e2e/1000.spec.js"];
+  const testingType = "e2e";
+  const record = true;
+
+  const summarizedResults = await run({
+    projectId,
+    key,
+    spec,
+    testingType,
+    record,
+  });
+
   console.log("summarizedResults");
   console.log(summarizedResults);
 })();
