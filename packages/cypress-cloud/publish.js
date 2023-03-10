6de6acac6221d8e103fdea806839da3bbb19cfb5
Andrew Goldis <agoldis@users.noreply.github.com>
2023-03-09 23:27:01 -0800

    Fix npm package TS and CJS exports (#70)
    * chore: release v1.4.2-beta.0
    
    * fix: use release script
    
    * chore: remove dep
    
    * fix: fix TS and CJS exports for published package
    
    * chore: release v1.4.2
    
    * chore: release v1.4.2
    
    * chore: restore smoke test

---
+++ packages/cypress-cloud/publish.js
@@ -0,0 +1,50 @@
+#!/usr/bin/env node
+
+const { execSync } = require("child_process");
+const fs = require("fs");
+const pkg = require("./package.json");
+
+const { Command, Option } = require("@commander-js/extra-typings");
+
+const program = new Command()
+  .name("publish")
+  .option("-t, --tag <beta | latest>", "npm dist-tag to publish to");
+
+program.parse(process.argv);
+const options = program.opts();
+
+console.log(options);
+if (!options.tag) {
+  console.log("No tag supplied: beta or latest");
+  process.exit(1);
+}
+
+const newPkg = {
+  ...pkg,
+  main: "./index.js",
+  files: ["*"],
+  exports: {
+    ".": "./",
+    "./plugin": {
+      import: "./plugin/index.js",
+      require: "./plugin/index.js",
+      types: "./plugin/index.d.ts",
+    },
+    "./support": {
+      import: "./support/index.js",
+      require: "./support/index.js",
+      types: "./support/index.d.ts",
+    },
+    "./package.json": "./package.json",
+  },
+};
+fs.copyFileSync("./LICENSE.md", "./dist/LICENSE.md");
+fs.writeFileSync(
+  "./dist/package.json",
+  JSON.stringify(newPkg, null, 2),
+  "utf-8"
+);
+execSync(`npm pack --dry-run && npm publish --tag ${options.tag}`, {
+  cwd: "./dist",
+  stdio: "inherit",
+});
