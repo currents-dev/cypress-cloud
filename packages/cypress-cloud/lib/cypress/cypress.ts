dabd843990d1862d656596308b36254cf537266a
Andrew Goldis <agoldis@users.noreply.github.com>
2023-03-26 22:59:01 -0700

    feat: Offline mode (#110)
    * chore: wip
    
    * feat: allow running in offline mode
    
    * chore: add offline e2e test
    
    * chore: add offline e2e test

--- packages/cypress-cloud/lib/cypress/cypress.ts
+++ packages/cypress-cloud/lib/cypress/cypress.ts
@@ -1,15 +1,34 @@
 import cypress from "cypress";
 import {
+  CurrentsRunParameters,
   CypressResult,
   ValidatedCurrentsParameters,
 } from "cypress-cloud/types";
 import Debug from "debug";
+import _ from "lodash";
 import { getCypressRunAPIParams } from "../config";
 
 const debug = Debug("currents:cypress");
 interface RunCypressSpecFile {
   spec: string;
 }
+
+export function runBareCypress(params: CurrentsRunParameters = {}) {
+  // revert currents params to cypress params
+  // exclude record mode params
+  const p = {
+    ...params,
+    ciBuildId: undefined,
+    tag: undefined,
+    parallel: undefined,
+    record: false,
+    group: undefined,
+    spec: _.flatten(params.spec).join(","),
+  };
+  debug("Running bare Cypress with params %o", p);
+  return cypress.run(p);
+}
+
 /**
  * Run Cypress tests, we need to pass down the stripped options as if we've received them from the CLI
  */
