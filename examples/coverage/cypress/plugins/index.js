1c3cd8b99757b615e5b49399bb3980ef0bc8ec7a
Andrew Goldis <agoldis@gmail.com>
2023-08-24 11:30:45 -0700

    chore: wip

--- examples/coverage/cypress/plugins/index.js
+++
@@ -1,8 +0,0 @@
-const { cloudPlugin } = require("cypress-cloud/plugin");
-module.exports = (on, config) => {
-  require("@cypress/code-coverage/task")(on, config);
-  // used to instrument code tested like unit tests
-  on("file:preprocessor", require("@cypress/code-coverage/use-babelrc"));
-  // call cypress-cloud after the @cypress/code-coverage plugin
-  return cloudPlugin(on, config);
-};
