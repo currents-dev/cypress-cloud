0e574657481722266f27722d4d9e038270aa5206
Andrew Goldis <agoldis@gmail.com>
2024-09-26 14:58:57 -0700

    chore: add cypress-terminal-report example

--- examples/happo/cypress.config.ts
+++ examples/happo/cypress.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from "cypress";
 import { cloudPlugin } from "cypress-cloud/plugin";
+
 // @ts-ignore
 import patchCypressOn from "cypress-on-fix";
 // @ts-ignore
 import * as happoTask from "happo-cypress/task";
+
+import installTerminal from "cypress-terminal-report/src/installLogsPrinter";
 
 module.exports = defineConfig({
   e2e: {
@@ -13,9 +16,13 @@ module.exports = defineConfig({
     specPattern: "cypress/*/**/*.spec.js",
     async setupNodeEvents(cyOn, config) {
       const on = patchCypressOn(cyOn);
+
+      installTerminal(on, {
+        printLogsToConsole: "always",
+      });
       happoTask.register(on, config);
+
       return await cloudPlugin(on, config);
     },
   },
-
 });
--- examples/happo/cypress/support/e2e.ts
+++ examples/happo/cypress/support/e2e.ts
@@ -1,3 +1,4 @@
+require("cypress-terminal-report/src/installLogsCollector")();
 require("cypress-cloud/support");
 require("./commands");
 
